a4f6feee43d1d9aa4e27ba5e10180b8172c72202
EC2 Default User <ec2-user@ip-172-31-12-21.ap-northeast-1.compute.internal>
2024-07-08 13:06:37 +0000

    編集画面まで作成

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,38 @@
 class ListsController < ApplicationController
   def new
+    # Viewファイルに渡すためのインスタンス変数に空のModelオブジェクトを生成する
+    @list = List.new
+  end
+
+  def create
+    # 1.&2.データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    #3.データをdbへ保存するためのsaveメソッド実行
+    list.save
+    # 4.トップ画面へリダイレクト
+    # redirect_to '/top'を書き換え
+    redirect_to list_path(list.id)
   end
 
   def index
+    # Listから全てを取り出す
+    @lists = List.all
   end
 
   def show
+    # Listからidに応じたものを取り出す
+    @list = List.find(params[:id])
   end
 
   def edit
+    # Listからidに応じたものを取り出す
+    @list = List.find(params[:id])
   end
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title,:body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+
+  # URL =>コントローラ#アクションの結び付け
   get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
+  get 'lists' => 'lists#index'
+  # 'lists/:id'にlistという名前を付ける
+  get 'lists/:id' => 'lists#show',as: 'list'
+  # 'lists/:id/edit'にedit_listという名前を付ける
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   post 'lists' => 'lists#create'
   get '/top' => 'homes#top'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
