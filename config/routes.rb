52e7adff2642d46ca4e352cc64c8fc4f08f1ee12
EC2 Default User <ec2-user@ip-172-31-12-21.ap-northeast-1.compute.internal>
2024-07-08 13:44:33 +0000

    更新画面まで作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   get 'lists' => 'lists#index'
   # 'lists/:id'にlistという名前を付ける
   get 'lists/:id' => 'lists#show',as: 'list'
-  # 'lists/:id/edit'にedit_listという名前を付ける
+  # 表示'lists/:id/edit'にedit_listという名前を付ける
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   post 'lists' => 'lists#create'
   get '/top' => 'homes#top'
+  # 更新機能'lists/:id'にupdate_listという名前を付ける
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
